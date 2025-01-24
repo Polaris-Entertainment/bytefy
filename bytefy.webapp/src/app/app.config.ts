d65c277b8fc45c02500cc02cbedd90e358c1f177
Myx <info@azaaxin.com>
2025-01-24 23:59:06 +0100

    Fix colorscheme

--- bytefy.webapp/src/app/app.config.ts
+++ bytefy.webapp/src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNgIconsConfig } from '@ng-icons/core';
 import { HttpClientXsrfModule, provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { bytefy_dark } from './bytefy-dark-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimationsAsync("animations"), 
+    providePrimeNG({ 
+      theme: {
+          preset: bytefy_dark
+      }
+    }),
     provideNgIconsConfig({
       size: '1.5em',
     }),
