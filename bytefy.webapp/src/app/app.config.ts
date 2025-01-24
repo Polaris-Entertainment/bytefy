fe935d101cead4f43308389d3f89537ef467d7cc
Myx <info@azaaxin.com>
2025-01-25 00:36:05 +0100

    v18

--- bytefy.webapp/src/app/app.config.ts
+++ bytefy.webapp/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNgIconsConfig } from '@ng-icons/core';
-import { HttpClientXsrfModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import { bytefy_dark } from './bytefy-dark-theme';
 
